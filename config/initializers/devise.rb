6625e135b2b0f94c1bc521b6e73d2b8bf7b946eb
azm555 <hy.hob.sub55@gmail.com>
2024-06-26 05:57:33 +0900

    devise_token_authによるUserモデルの作成

---
+++ config/initializers/devise.rb
@@ -0,0 +1,7 @@
+Devise.setup do |config|
+  # ==> ORM configuration
+  # Load and configure the ORM. Supports :active_record (default) and
+  # :mongoid (bson_ext recommended) by default. Other ORMs may be
+  # available as additional gems.
+  require 'devise/orm/active_record'
+end
