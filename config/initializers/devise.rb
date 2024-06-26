3e3d5c41e65a6baf18837d8580e8e2633b9dd225
azm555 <hy.hob.sub55@gmail.com>
2024-06-27 06:41:55 +0900

    GitHub Actionsの導入及びrubocopによるエラー修正

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,7 +1,7 @@
-Devise.setup do |config|
+Devise.setup do |_config|
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default) and
   # :mongoid (bson_ext recommended) by default. Other ORMs may be
   # available as additional gems.
-  require 'devise/orm/active_record'
+  require "devise/orm/active_record"
 end
