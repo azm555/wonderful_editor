513f2b2af49f36b9e9c93bcfa757842ce17761e8
azm555 <hy.hob.sub55@gmail.com>
2024-08-27 21:04:41 +0900

    自身の下書き記事及び公開記事の取得機能の修正

--- app/controllers/api/v1/current/articles_controller.rb
+++ app/controllers/api/v1/current/articles_controller.rb
@@ -1,7 +1,9 @@
 class Api::V1::Current::ArticlesController < Api::V1::BaseApiController
+  before_action :authenticate_user!
+
   def index
-    # 下書きの記事を更新日順に取得する
-    @articles = Article.published.order("updated_at DESC")
+    # 公開記事を更新日順に取得する
+    @articles = current_user.articles.published.order("updated_at DESC")
     # binding.pry
     render json: @articles, each_serializer: Api::V1::ArticleSerializer
     # renderメソッド使用時に、デフォルトではない上記シリアライザーを指定した
